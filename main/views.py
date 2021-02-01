fd102cd4c9d62028e8f42d94da4841e5534c0713
Aitolkun-tech <elik@example.com>
2021-02-01 16:50:25 +0600

    pulled data out of base on test site

--- main/views.py
+++ main/views.py
@@ -5,8 +5,8 @@ def homepage(request):
     return render(request, 'index.html')
 
 def test(request):
-    todo_list = ToDo.objects()
-    return render(request, 'test.html', {"todo_list: todo_list"})
+    todo_list = ToDo.objects.all()
+    return render(request, 'test.html', {"todo_list": todo_list})
 
 def second(request):
       return HttpResponse('Test 2 page')    
